e437e2074697e8ebab9bbef91cfdcf2c5e410fe2
kenmar7in <kenmar7in@yandex.com>
2022-04-22 19:45:34 +0300

    added grid

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,42 @@
-//
+// Menu
+
+const TOGGLE_CLOSED_CLASS = 'menu-toggle--closed';
+const TOGGLE_OPENED_CLASS = 'menu-toggle--opened';
+const TOGGLE_NOJS_CLASS = 'menu-toggle--nojs';
+const MENU_NOJS_CLASS = 'menu--nojs';
+const DESKTOP_WIDTH = '(min-width: 1234px)';
+
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+const desktop = window.matchMedia(DESKTOP_WIDTH);
+
+menuToggle.classList.remove(TOGGLE_NOJS_CLASS);
+menu.classList.remove(MENU_NOJS_CLASS);
+
+function showMenu() {
+  menuToggle.classList.remove(TOGGLE_CLOSED_CLASS);
+  menuToggle.classList.add(TOGGLE_OPENED_CLASS);
+  menu.style.display = 'flex';
+}
+
+function hideMenu() {
+  menuToggle.classList.remove(TOGGLE_OPENED_CLASS);
+  menuToggle.classList.add(TOGGLE_CLOSED_CLASS);
+  menu.style.display = 'none';
+}
+
+function onMenuToggleClick() {
+  const isMenuClosed = this.classList.contains(TOGGLE_CLOSED_CLASS);
+
+  isMenuClosed ? showMenu() : hideMenu();
+}
+
+menuToggle.addEventListener('click', onMenuToggleClick);
+
+function onWindowResize() {
+  const isDesktopMatches = desktop.matches;
+
+  isDesktopMatches ? showMenu() : hideMenu();
+}
+
+window.addEventListener("resize", onWindowResize);
